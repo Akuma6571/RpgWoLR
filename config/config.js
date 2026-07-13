158d61198609276238bfb3648b2094c0f590a8ee
Akuma6571 <foxybr6571@gmail.com>
2026-07-13 00:29:36 -0300

    Criar config/config.js

---
+++ config/config.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+module.exports = {
+
+    BOT_TOKEN: process.env.BOT_TOKEN,
+
+    CLIENT_ID: process.env.CLIENT_ID,
+
+    GUILD_ID: process.env.GUILD_ID,
+
+    DATABASE_URL: process.env.DATABASE_URL,
+
+    EMBED_COLOR: "#6b0000",
+
+    BOT_NAME: "World Engine",
+
+    VERSION: "1.0.0"
+
+};
