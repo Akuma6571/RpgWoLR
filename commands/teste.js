f0d59e1e1bb1f117b58c0ff67aeb11d987252a74
Akuma6571 <foxybr6571@gmail.com>
2026-09-04 01:23:57 -0300

    Remover arquivo de teste

--- commands/teste.js
+++
@@ -1,2 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-module.exports = { data: new SlashCommandBuilder().setName('teste').setDescription('Teste'), async execute(interaction) { await interaction.reply('ok'); } };
